5d98edf3041cd56af564305f9b90ced799f1e395
Seth Kerr <skerr@aggies.ncat.edu>
2018-10-05 22:09:50 -0400

    commit to merge current files to master

--- get_dnac_devices.py
+++ get_dnac_devices.py
@@ -33,14 +33,23 @@ def network_device_list(dnac, token):
     headers["x-auth-token"] = token
     response = requests.get(url, headers=headers, verify=False)
     data = response.json()
+    dnac_dict = dict()
+    dnac_dict['hostname']==[]
+    dnac_dict['platformId']==[]
+    dnac_dict['softwareType']==[]
+    dnac_dict['softwareVersion']==[]
+    dnac_dict['upTime']==[]
     for item in data['response']:
         dnac_devices.add_row([item["hostname"],item["platformId"],item["softwareType"],item["softwareVersion"],item["upTime"]])
-
+    for key in dnac_dict:
+	if dnac_dict[key]==dnac_devices.get(item):
+		dnace_dict[key].append(dnac_devices)
+	
 
 login = dnac_login(dnac["host"], dnac["username"], dnac["password"])
 network_device_list(dnac, login)
 
 print(dnac_devices)
 
-with open('dnac_devices.json', 'w') as outfile:
-	json.dump(dnac_devices, outfile)
+with open('dnac_dict.json', 'w') as outfile:
+	json.dump(dnac_dict, outfile)
